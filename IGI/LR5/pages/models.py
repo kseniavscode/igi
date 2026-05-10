fbaee99610b931447a75915d9a9e547a3e7e1906
ksenia <ms.ks2007@mail.ru>
2026-05-10 20:01:14 +0300

    added classes about company, start page,

--- IGI/LR5/pages/models.py
+++ IGI/LR5/pages/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+
+class CompanyInfo(models.Model):
+
+    name = models.CharField(max_length=200, verbose_name="Company name")
+    description = models.TextField(verbose_name="Company description")
+    history = models.TextField(verbose_name="History", blank=True)
+    requisites = models.TextField(verbose_name="Details", blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Article(models.Model):
+    """News"""
+
+    title = models.CharField(max_length=100, verbose_name="Title of article")
+    short_description = models.CharField(max_length=500, verbose_name="Short part of article")
+    full_text = models.CharField(max_length=1500, verbose_name="Text of article")
+    image = models.ImageField(upload_to='news/', null=True, blank=True)
+    publishing_date = models.DateField(auto_now_add=True, verbose_name="Date of publishing")
+
+    def __str__(self):
+        return self.title
+    
+class FAQ(models.Model):
+
+    question = models.CharField(max_length=200, verbose_name="Question/Trem")
+    answer = models.CharField(max_length=400, verbose_name="Answer")
+    date = models.DateField(auto_now_add=True, verbose_name="Date of the last changing")
+
+    def __str__(self):
+        return self.question
+class Employee(models.Model):
+    full_name = models.CharField(max_length=150, verbose_name="Full- Last- Second name")
+    position = models.CharField(max_length=100, verbose_name="Position in this company")
+    email = models.EmailField(verbose_name="Email")
+    photo = models.ImageField(upload_to='employees/', null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+    
+class Vacancy(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Vacancy")
+    description = models.CharField(max_length=1000, verbose_name="Description")
+
+    def __str__(self):
+        return self.title
+    
+class Review(models.Model):
+    
+    name = models.CharField(max_length=100)
+    rating = models.PositiveIntegerField()
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    
+class PromoCode(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discode = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.code
--- IGI/LR5/pages/views.py
+++ IGI/LR5/pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Article, CompanyInfo, FAQ, Employee, Vacancy, Review, PromoCode
 
 # Create your views here.
+
+def home_page(request):
+    """Main page with the last article"""
+    last_article = Article.objects.order_by('-publishing_date').first()
+    return render(request, 'pages/home.html', {'article': last_article})
