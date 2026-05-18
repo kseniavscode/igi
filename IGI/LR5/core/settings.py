ab2fdf6c8cbeb7583abb7eb5a8c531fd2606b87e
ksenia <ms.ks2007@mail.ru>
2026-05-18 10:27:10 +0300

    added p-db and try json

--- IGI/LR5/core/settings.py
+++ IGI/LR5/core/settings.py
@@ -91,7 +91,12 @@ DATABASES = {
         conn_max_age=600
     )
 }
-
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600
+    )
 
 
 # Password validation
